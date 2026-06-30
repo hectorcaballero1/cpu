150bb9bf9ef6c457553a634e44863bbe1228584c
hector <hectorcaballero1205@gmail.com>
2026-06-30 07:43:55 -0500

    expand imem to 128 words for rv32i heapsort

--- george/imem.v
+++ george/imem.v
@@ -1,13 +1,13 @@
 module imem(input  [31:0] a,
             output [31:0] rd);
 
-    reg [15:0] RAM[127:0];   // 128 halfwords = 64 words
+    reg [15:0] RAM[255:0];   // 256 halfwords = 128 words
 
-    reg [31:0] WORDS[63:0];
+    reg [31:0] WORDS[127:0];
     integer i;
     initial begin
         $readmemh("p1.mem", WORDS);
-        for (i = 0; i < 64; i = i + 1) begin
+        for (i = 0; i < 128; i = i + 1) begin
             RAM[2*i]   = WORDS[i][15:0];
             RAM[2*i+1] = WORDS[i][31:16];
         end
